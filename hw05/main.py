b7246e530e5d1044c670d2c7c8d7f5f2b7550f55
HLGBAT <1269689465@qq.com>
2017-10-16 21:38:39 +0800

    Update main.py

--- hw05/main.py
+++ hw05/main.py
@@ -1,1 +1,15 @@
 
+#我的电子邮件tom@gamil.com。
+#将什么发送到jerry123@163.com或者3123432@qq.com.
+#若遇特殊情况打电话给18212344567。
+import re
+
+Message='''我的电子邮件tom@gamil.com。
+将什么发送到jerry123@163.com或者3123432@qq.com.
+若遇特殊情况打电话给18212344567。'''
+
+Emails = re.findall(r'[a-zA-Z0-9]+@[a-z0-9]+\.com',Message)
+phonenumbers = re.findall(r'\d{11}',Message)
+print(Emails)\
+              
+print(phonenumbers)
